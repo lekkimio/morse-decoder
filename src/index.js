545513b51ea2f52294b4693ebae9811328e3e9a2
lekkimio <70840990+lekkimio@users.noreply.github.com>
2020-10-25 17:24:26 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.substring(i, i + 10));
+    }
+
+    let arr2 = [];
+    arr.forEach(function (element) {
+        if (element === '**********') {
+            arr2.push(' ');
+        } else {
+            let arr3 = [];
+            for (i = 0; i < element.length; i += 2) {
+                if (element[i] + element[i + 1] === '00') {
+                } else if (element[i] + element[i + 1] === '10') {
+                    arr3.push('.');
+                } else {
+                    arr3.push('-');
+                }
+            }
+            arr2.push(arr3);
+        }
+    });
+    let arr3 = [];
+    arr2.forEach(function (element) {
+        if (element !== ' ') {
+            arr3.push(element.join(''));
+        } else {
+            arr3.push(element);
+        }
+    });
+
+    let arr4 = [];
+    arr3.forEach(function (element) {
+        if (element !== ' ') {
+            arr4.push(MORSE_TABLE[element]);
+        } else {
+            arr4.push(element);
+        }
+    });
+    return arr4.join('');
 }
 
 module.exports = {
